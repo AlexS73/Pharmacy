ed888367cc217795229271e4b77a656524754680
Alexey Skolotnev <skolotnevalex@outlook.com>
2022-11-16 11:43:55 +0300

    models

---
+++ Pharmacy.Core/Models/AuthenticateResponse.cs
@@ -0,0 +1,28 @@
+﻿using Pharmacy.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Core.Models
+{
+    public class AuthenticateResponse
+    {
+        public AuthenticateResponse(User user, string jwtToken, string refreshToken)
+        {
+            Id = user.Id;
+            UserName = user.UserName;
+            JwtToken = jwtToken;
+            RefreshToken = refreshToken;
+        }
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string JwtToken { get; set; }
+
+        [JsonIgnore] //возвращается в http only cookie
+        public string RefreshToken { get; set; }
+    }
+}
